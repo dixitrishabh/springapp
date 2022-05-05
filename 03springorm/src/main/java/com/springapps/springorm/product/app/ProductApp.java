0c6efbce1f9cc1d1c620538248b557e50ae77da9
Dixit <therishabhdixit01@gmail.com>
2022-05-05 10:29:07 +0530

    springormreadop

--- 03springorm/src/main/java/com/springapps/springorm/product/app/ProductApp.java
+++ 03springorm/src/main/java/com/springapps/springorm/product/app/ProductApp.java
@@ -2,6 +2,7 @@ package com.springapps.springorm.product.app;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.List;
 
 import com.springapps.springorm.product.dao.ProductDao;
 import com.springapps.springorm.product.entity.Product;
@@ -17,16 +18,58 @@ public class ProductApp
     	ApplicationContext context = new ClassPathXmlApplicationContext("com/springapps/springorm/product/config/config.xml");
 		ProductDao productdao = (ProductDao) context.getBean("productDaoImpl");
 		
-		createProduct(productdao);
+//		createProduct(productdao, 1, "headphones", "noise cancelling", 250);
+//		createProduct(productdao, 2, "smartphone", "Android phone", 8000);
+//		createProduct(productdao, 3, "smartwatch", "big display", 15000);
+		
+//		createProduct(productdao);
+		
+//		updateProduct(productdao);
+		
+//		deleteProduct(productdao);
+		
+//		findProduct(productdao,2);
+		
+		findAllProducts(productdao);
+	}
+    
+	private static void findAllProducts(ProductDao productdao) {
+		List<Product> products = productdao.findAll();
+		System.out.println(products);
 	}
 
-	private static void createProduct(ProductDao productdao) {
+	private static void findProduct(ProductDao productdao, int id) {
+		Product product = productdao.find(id);
+		System.out.println(product);
+	}
+    
+    private static void deleteProduct(ProductDao productdao) {
+		Product product = new Product();
+		product.setId(1);
+		productdao.delete(product);
+		System.out.println("Product deleted.");
+	}
+
+	private static void updateProduct(ProductDao productdao) {
 		Product product = new Product();
 		product.setId(1);
 		product.setName("iWatch");
 		product.setDescription("best watch");
-		product.setPrice(21560);
+		product.setPrice(18000);
+		productdao.update(product);
+		System.out.println("Row updated.");
+	}
+
+
+	private static void createProduct(ProductDao productdao) {
+		Product product = new Product();
+		product.setId(2);
+		product.setName("iTV");
+		product.setDescription("best TV");
+		product.setPrice(21562);
 		int result = productdao.create(product);
 		System.out.println("Number of rows inserted: " + result);
 	}
+	
+	
 }
--- 03springorm/src/main/java/com/springapps/springorm/product/dao/impl/ProductDaoImpl.java
+++ 03springorm/src/main/java/com/springapps/springorm/product/dao/impl/ProductDaoImpl.java
@@ -1,7 +1,7 @@
 package com.springapps.springorm.product.dao.impl;
 
 import java.io.Serializable;
-
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -22,6 +22,30 @@ public class ProductDaoImpl implements ProductDao {
 	public int create(Product product) {
 		Integer result  = (Integer) hibernateTemplate.save(product);
 		return result;
+	}
+
+	@Override
+	@Transactional
+	public void update(Product product) {
+		hibernateTemplate.update(product);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Product product) {
+		hibernateTemplate.delete(product);
+	}	
+	
+	@Override
+	public Product find(int id) {
+		Product product = hibernateTemplate.get(Product.class, id);
+		return product;
+	}
+
+	@Override
+	public List<Product> findAll() {
+		List<Product> products = hibernateTemplate.loadAll(Product.class);
+		return products;
 	}
 
 }
