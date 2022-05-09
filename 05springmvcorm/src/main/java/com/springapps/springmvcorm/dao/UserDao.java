1a91125a33b7ee1ff25491ba09b41fafbaca7609
Dixit <therishabhdixit01@gmail.com>
2022-05-09 10:19:31 +0530

    spring05

---
+++ 05springmvcorm/src/main/java/com/springapps/springmvcorm/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.springapps.springmvcorm.dao;
+
+import java.util.List;
+
+import com.springapps.springmvcorm.entity.User;
+
+public interface UserDao {
+
+	int create (User user);
+
+	List<User> findUsers();
+	
+}
